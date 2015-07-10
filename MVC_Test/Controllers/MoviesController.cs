8e229c52ea809ac1312f3626c86db83e6023df73
Matthew Fosberry <matthew@intuitivesystems.co.uk>
2015-07-10 15:44:18 +0100

    Initial COmmit

---
+++ MVC_Test/Controllers/MoviesController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Test.Models;
+
+namespace MVC_Test.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private MovieDBContext db = new MovieDBContext();
+
+        //
+        // GET: /Movies/
+
+        public ActionResult Index()
+        {
+            return View(db.Movies.ToList());
+        }
+
+        //
+        // GET: /Movies/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Movie movies = db.Movies.Find(id);
+            if (movies == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movies);
+        }
+
+        //
+        // GET: /Movies/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Movies/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Movie movies)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Movies.Add(movies);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(movies);
+        }
+
+        //
+        // GET: /Movies/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Movie movies = db.Movies.Find(id);
+            if (movies == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movies);
+        }
+
+        //
+        // POST: /Movies/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Movie movies)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(movies).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(movies);
+        }
+
+        //
+        // GET: /Movies/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Movie movies = db.Movies.Find(id);
+            if (movies == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movies);
+        }
+
+        //
+        // POST: /Movies/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Movie movies = db.Movies.Find(id);
+            db.Movies.Remove(movies);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
